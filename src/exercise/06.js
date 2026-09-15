6d00a4e64bb628f57bb72615f149315338c58527
Loza Demeke <your-github-email@example.com>
2026-09-16 02:04:34 +0300

    Complete exercise 05

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -3,14 +3,19 @@
 
 import * as React from 'react'
 
+function formatMediaDebugValue({query, state}) {
+  return `${query} => ${state}`
+}
+
 function useMedia(query, initialState = false) {
   const [state, setState] = React.useState(initialState)
-  // 🐨 call React.useDebugValue here.
-  // 💰 here's the formatted label I use: `\`${query}\` => ${state}`
+
+  React.useDebugValue({query, state}, formatMediaDebugValue)
 
   React.useEffect(() => {
     let mounted = true
     const mql = window.matchMedia(query)
+
     function onChange() {
       if (!mounted) {
         return
@@ -34,9 +39,24 @@ function Box() {
   const isBig = useMedia('(min-width: 1000px)')
   const isMedium = useMedia('(max-width: 999px) and (min-width: 700px)')
   const isSmall = useMedia('(max-width: 699px)')
-  const color = isBig ? 'green' : isMedium ? 'yellow' : isSmall ? 'red' : null
 
-  return <div style={{width: 200, height: 200, backgroundColor: color}} />
+  const color = isBig
+    ? 'green'
+    : isMedium
+      ? 'yellow'
+      : isSmall
+        ? 'red'
+        : null
+
+  return (
+    <div
+      style={{
+        width: 200,
+        height: 200,
+        backgroundColor: color,
+      }}
+    />
+  )
 }
 
 function App() {
